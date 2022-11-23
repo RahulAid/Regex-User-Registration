3ab51c76e60ec54d4055b436ffcaaf89715373d3
RahulAid <raccess97@gmail.com>
2022-11-24 00:05:39 +0530

    Validate First Name

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Regex_User_Registration
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("User Registration Using Regex");
+
+            UserRegistration objUser = new UserRegistration();
+            objUser.FirstName();
+        }
+    }
+}
---
+++ UserRegistration.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+public class UserRegistration
+{
+    public void FirstName()
+    {
+        Console.Write("\nEnter First Name: ");
+        string firstName = Console.ReadLine();
+        var regex = new Regex(@"^[A-Z]{1}[a-z]{2,}$");
+        bool matchRes = regex.IsMatch(firstName);
+        if (matchRes == true)
+        {
+            Console.WriteLine($"{firstName} is valid");
+        }
+        else
+        {
+            Console.WriteLine($"{firstName} is invalid");
+            FirstName();
+        }
+    }
+}
